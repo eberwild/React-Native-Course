a6453aa94b5b84dcba4a24b9a30bdf5eea4186f9
Kevin <kevinflotow@arcor.de>
2026-03-17 20:25:06 +0100

    touchables introduction

--- App.js
+++ App.js
@@ -4,15 +4,28 @@
 //      -> onPress() is the same like onClick()
 // Image -> <Image source={require('./assets/icon.png')} for intern images
 //       -> <Image source={{ uri: 'https://... , witdh: 200 , height: 300}} for external images we need to specify the dimensions!
+// Touchable -> only can contain 1 single child element
+//              TouchableWithoutFeedback (does nothing) , 
+//              TouchableOpacity (reduces opacity for a short time) ,
+//              TouchableHighlight ('focuses' the image if hold)
 
-import { StyleSheet, Text, View , Image} from 'react-native';
+import { StyleSheet, 
+         Text, 
+         View , 
+         Image, 
+         TouchableHighlight,
+         TouchableNativeFeedback} from 'react-native';
 // SafeAreaView -> content gets margin top to not be outside of the screen or coverd by the camera
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
 
   const handlePress = () => {
-    console.log('Text was pressed');
+    console.log('Text was pressed.');
+  }
+
+  const handleTap = () => {
+    console.log('Image was tapped.')
   }
 
 
@@ -23,16 +36,21 @@ export default function App() {
         Hello lets see what happens when a text is very very very 
         large so we will have to set the number of lines
       </Text>
-      <Image 
-        resizeMode='cover'
-        blurRadius={0}
-        fadeDuration={3000}
-        source={{ 
-          uri: 'https://picsum.photos/200/300',
-          width: 200,
-          height: 300
-          }}
-      />
+      <TouchableHighlight onPress={() => handleTap()}>
+        <Image 
+          resizeMode='cover'
+          blurRadius={0}
+          fadeDuration={3000}
+          source={{ 
+            uri: 'https://picsum.photos/200/300',
+            width: 200,
+            height: 300
+            }}
+        />
+      </TouchableHighlight>
+      <TouchableNativeFeedback>
+        <View style={{ width: 200 , height: 300 , backgroundColor: 'dodgerblue'}}></View>
+      </TouchableNativeFeedback>
     </SafeAreaView>
   );
 }
